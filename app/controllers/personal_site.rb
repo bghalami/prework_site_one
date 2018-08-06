0ef1aa42c032e90f681b5db9845051e582c2d9ec
bghalami <benjaminghalami@gmail.com>
2018-08-06 15:23:07 -0600

    adding all sorts of things

---
+++ app/controllers/personal_site.rb
@@ -0,0 +1,10 @@
+require 'rack'
+require 'pry'
+# PersonalSite
+class PersonalSite
+  def self.call(env)
+    # Recall, this array includes the HTTP response status code,
+    # HTTP response headers & HTTP body
+    ['200', { 'Content-Type' => 'text/html' }, ['Welcome!']]
+  end
+end
